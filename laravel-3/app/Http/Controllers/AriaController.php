f854930987cdf8698f37b84b1c23d609756d7b79
s-a-rafi <sarafi3258@gmail.com>
2025-10-20 08:04:03 +0600

    up

---
+++ laravel-3/app/Http/Controllers/AriaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aria;
+use Illuminate\Http\Request;
+
+class AriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $arial = Aria::find(1);
+        return $arial->ticket();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Aria $aria)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Aria $aria)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Aria $aria)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Aria $aria)
+    {
+        //
+    }
+}
